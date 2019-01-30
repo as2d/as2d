0a318d3d34a2a603a78d890bdd30146c00e45ca0
jtenner <tenner.joshua@gmail.com>
2019-01-30 18:29:12 -0500

    add coveralls

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,10 @@ module.exports = {
   // clearMocks: false,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+    "src/**/*.ts"
+  ],
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: null,
